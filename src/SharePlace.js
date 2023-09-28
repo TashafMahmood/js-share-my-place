3fd8663878511120e30bf0cff9383a25dae7ffa8
pershiba-velusamy06 <pershiba@elred.io>
2023-09-28 18:17:01 +0530

    map

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,21 +1,39 @@
+import{Modal} from './UI/modal'
+import {Map} from './UI/Map'
+
 class PlaceFinder {
     constructor() {
         const addressForm = document.querySelector('form');
         const locateUserButton = document.getElementById('locate-btn');
-        locateUserButton.addEventListener('click', this.locateUserHandlers)
+        locateUserButton.addEventListener('click', this.locateUserHandlers.bind(this))
         addressForm.addEventListener('submit', this.findUserAddressHandler)
     }
 
+
+    selectPlce(coordinates){
+        if(this.map){
+this.map.render(coordinates)
+        }else{
+            this.map=new Map(coordinates)
+        }
+
+    }
+
+    
     locateUserHandlers() {
         if (!navigator.geolocation) {
             alert('location feature in not available in your browser - Please some other mordern browser');
             return
         }
+        const modal = new Modal('loading-modal-content','Loading content.. Please wait')
+        modal.showModal()
         navigator.geolocation.getCurrentPosition(successResult=>{
+            modal.hideModal() 
             const coordinates={
                 lat:successResult.coords.latitude,
-                long:successResult.coords.longitude
+                lng:successResult.coords.longitude
             }
+            this.selectPlce(coordinates)
             console.log(coordinates,"coordinates")
         },error=>{
             alert('Could not locate you unfortunately Please enter address manually')
--- src/UI/modal.js
+++ src/UI/modal.js
@@ -1,13 +1,33 @@
-class Modal{
-    constructor(contentId){
+export class Modal {
+    constructor(contentId, fallBackText) {
+        this.fallBackText = fallBackText;
+        this.contentTemplateEle = document.getElementById(contentId);
+        this.modalTemplateEle = document.getElementById('modal-template');
 
+    }
+
+    showModal() {
+        if ('content' in document.createElement('template')) {
+            this.modalElements = document.importNode(this.modalTemplateEle.content, true)
+            this.modelElement = this.modalElements.querySelector('.modal')
+            this.backDropElements = this.modalElements.querySelector('.backdrop')
+            this.contentElements = document.importNode(this.contentTemplateEle.content, true)
+            this.modelElement.append(this.contentElements);
+            document.body.insertAdjacentElement('afterbegin',this.modelElement)
+            document.body.insertAdjacentElement('afterbegin',this.backDropElements)
+        } else {
+            alert(this.fallBackText)
+        }
 
     }
 
-    showModal(){
-
-    }
-    hideModal(){
+    hideModal() {
+        if(this.modelElement){
+            document.body.removeChild(this.modelElement)
+            document.body.removeChild(this.backDropElements)
+            this.modalElement=null;
+            this.backDropElements=null
+        }
 
     }
 }
