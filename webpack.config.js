971486c063fdff573815dd484228cf0784eb3758
pershiba-velusamy06 <pershiba@elred.io>
2023-09-28 22:14:23 +0530

    module

--- webpack.config.js
+++ webpack.config.js
@@ -12,28 +12,28 @@ module.exports = {
     path: path.resolve(__dirname, 'dist', 'assets', 'scripts'),
     publicPath: 'assets/scripts/'
   },
-  devtool: 'cheap-source-map',
-  // devServer: {
-  //   contentBase: './dist'
-  // },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.m?js$/,
-  //       exclude: /(node_modules)/,
-  //       use: {
-  //         loader: 'babel-loader',
-  //         options: {
-  //           presets: [
-  //             [
-  //               '@babel/preset-env',
-  //               { useBuiltIns: 'usage', corejs: { version: 3 } }
-  //             ]
-  //           ]
-  //         }
-  //       }
-  //     }
-  //   ]
-  // },
-  // plugins: [new CleanPlugin.CleanWebpackPlugin()]
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                { useBuiltIns: 'usage', corejs: { version: 3 } }
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()]
 };
